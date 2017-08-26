7ba299aed8a280e5a12b17fa8183250504e98c0b
andrejristevski <andrej.rist@gmail.com>
2017-08-26 21:34:18 +0200

    Adding new currencies

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ from datetime import date, timedelta
 from flask_cors import CORS, cross_origin
 
 
-repo = mongoRepo.Repo()
+currencyConfig = {
+            'metadataCollection' : 'metaData',
+            'ratesCollection' : "rates",
+            'dbName' : 'pytdb',
+            'currency': 'EUR'
+        }
+
+repo = mongoRepo.Repo(currencyConfig)
 service = dbService.DbService(repo)
 
 app = Flask(__name__)
@@ -27,12 +34,15 @@ def index () :
 
     startDate = request.args.get('startDate')
     endDate = request.args.get('endDate')
+    inpCur=request.args.get('inp')
+    outCur=request.args.get('out')
+
 
     d1= datetime.datetime(int(startDate[:4]), int(startDate[5:7]), int(startDate[8:]))
     d2= datetime.datetime(int(endDate[:4]), int(endDate[5:7]), int(endDate[8:]))
     res = service.getRatesBetweenDates(d1,d2)
     preparePayLoad(res)
-    print 'returnig sth'
+    print("returning dates bettween", startDate, "and", endDate)
     return jsonify(res)
 
 if __name__ == "__main__" :
